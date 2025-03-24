87725d3140bee503030c0bab4102ad79209ab5ef
Jones <2944106+c-bebop@users.noreply.github.com>
2025-03-24 15:28:45 +0100

    cache line size ist consexpr

--- include/dhb/block.h
+++ include/dhb/block.h
@@ -20,7 +20,7 @@ inline index_type tombstoneIndex() { return static_cast<index_type>(-2); };
 
 // TODO: This could take the entry size into account.
 inline bool uses_htab(size_t const bsize) {
-    const size_t cache_line = 64;
+    size_t constexpr cache_line = 64;
     // Each entry is typically 16 bytes or so. Preliminary experiments show that
     // using the hash index is only useful if the block becomes larger than
     // several cache lines (approx. 16 or so). Hence, the following is a
